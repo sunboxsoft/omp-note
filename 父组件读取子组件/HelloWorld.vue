c72b5fe490cf76d3d999eb1180446c83dadd4749
sunboxsoft <sunboxsoft@126.com>
2022-03-28 17:16:51 +0800

    first commit

---
+++ 父组件读取子组件/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 ref="testH2">Essential Links{{ new Date() }}</h2>
+    <button title="haha" @click="btnChange">按钮</button>
+
+    <TestA ref="refTestA"></TestA>
+  </div>
+</template>
+
+<script>
+import TestA from "@/components/TestA";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  components: {
+    TestA,
+  },
+  methods: {
+    btnChange(e) {
+      // 两种调用子组件中的属性和方法
+      // 方法一
+      console.log(this.$children[0].msg);
+      this.$children[0].refresh();
+      // 方法二
+      console.log(e.target.msg);
+      this.$refs.refTestA.refresh();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="" setup>
+h2 {
+  color: pink;
+}
+</style>
---
+++ 父组件读取子组件/TestA.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="TestA">测试ref</section>
+</template>
+
+<script>
+export default {
+  name: "TestA",
+  data() {
+    return {
+        msg:"sssssssssssssss"
+    };
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {
+    refresh() {
+        alert("AAAAAAAAAAAAAr")
+    },
+  },
+};
+</script>
+
+<style>
+</style>
